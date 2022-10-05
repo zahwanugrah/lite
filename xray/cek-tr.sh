76e8f179ca97c49d99126282080bdd2dc3b97b94
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-10-05 23:34:23 +0700

    Update cek-tr.sh

--- xray/cek-tr.sh
+++ xray/cek-tr.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
@@ -58,4 +54,4 @@ rm -rf /tmp/other.txt
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 
-trojan-menu
+menu
