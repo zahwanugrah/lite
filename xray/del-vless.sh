3c477e30f7d841d9617c0ba17aa909f292930efb
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-10-05 23:36:42 +0700

    Update del-vless.sh

--- xray/del-vless.sh
+++ xray/del-vless.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
 
 red='\e[1;31m'
 green='\e[0;32m'
@@ -56,5 +52,5 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
     
-    v2ray-menu
+    menu
     fi
