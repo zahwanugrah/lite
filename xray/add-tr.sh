99b66178a24a92d91441f69b75ae9b2427047254
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-10-05 23:33:26 +0700

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
@@ -71,4 +68,4 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
 
-trojan-menu
+menu
