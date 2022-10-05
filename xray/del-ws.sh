ea9cb724d31be25f398f3a11358ab3e41242d1a5
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-10-05 23:37:05 +0700

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
 
 red='\e[1;31m'
 green='\e[0;32m'
@@ -54,5 +51,5 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
     
-    v2ray-menu
+    menu
     fi
