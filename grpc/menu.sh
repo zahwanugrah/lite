9eb044e6085c100709b324a8d2d03aa12ce75745
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-07 03:43:44 +0700

    Create menu.sh

---
+++ grpc/menu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+Lred='\e[91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+CYAN='\e[36m'
+LIGHT='\033[0;37m'
+
+clear
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m                 ⇱  GRPC VMESS MENU  ⇲                    \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo -e " $blue[•1]$NC Create Grpc VMESS Accounts                                   "
+echo -e " $blue[•2]$NC Delete Grpc VMESS Accounts                                   "
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " $blue[•x]$NC Main Menu                                                        " 
+read -p "     Select From Options [1-2] :  " menu
+case $menu in
+1)
+clear
+add-tr
+;;
+2)
+clear
+del-tr
+exit
+;;
+*)
+clear
+menu
+;;
+esac
