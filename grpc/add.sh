02f02b352026b98e1be57ad816004cafc6e72e48
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-07 03:25:09 +0700

    Create add.sh

---
+++ grpc/add.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#########################
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m           ⇱ XRAY GRPC ⇲           \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+domain=$(cat /etc/xray/domain)
+tls=$(cat /etc/xray/grpc/vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/grpc/vmessgrpc.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+masaaktif="100000"
+#dom=$sub.$domain
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/grpc/vmessgrpc.json << EOF
+      {
+      "v": "0",
+      "ps": "${user}",
+      "add": "${dom}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "grpc",
+      "type": "none",
+      "host": "${sni}",
+      "tls": "tls"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/grpc/$user-tls.json)"
+systemctl restart xray.service
+systemctl restart vmess-grpc.service
+service cron restart
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m           ⇱ XRAY GRPC ⇲           \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Remarks           : ${user}"
+echo -e "Domain            : ${domain}"
+echo -e "Port VMess        : ${tls}"
+echo -e "ID                : ${uuid}"
+echo -e "Alter ID          : 0"
+echo -e "Mode              : Gun"
+echo -e "Security          : TLS"
+echo -e "Type              : grpc"
+echo -e "Service Name      : grpc"
+echo -e "SNI               : $sni"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link VMess GRPC  : "
+echo -e "${vmesslink1}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
