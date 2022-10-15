aa88d5eb66f650c44a7ab9ab873f33059906124c
rullpqh_ <114976790+rullpqh@users.noreply.github.com>
2022-10-15 18:21:28 +0700

    Update ins.sh

--- commit/ins.sh
+++ commit/ins.sh
@@ -2,6 +2,18 @@
 # / / Autoscript installer Lite Version
 # / / By Bhoikfostyahya
 
+# / / Make Collor 
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
 echo -e "[ ${green}INFO$NC ] Make Folder XRay ..."
 # / / Make Folder XRay
 mkdir -p /var/log/xray
